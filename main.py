ee87ff2756797f752c121e8aaba6981708039008
lucas.oliveira <lucasdinizoliveira2@gmail.com>
2025-06-09 12:44:05 -0500

    database connection test

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
 import firebase_admin
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, db
 from NeuedaHackathon.Data.data import process_data, get_data
 from NeuedaHackathon.Model.model import get_model, get_accuracy, get_confusion_matrix
 
-# Load the data
-X_train, X_test, y_train, y_test = process_data("bank_loan.csv")
+# # Load the data
+# X_train, X_test, y_train, y_test = process_data("bank_loan.csv")
+#
+# # Load or make the model
+# model = get_model(data_path="bank_loan.csv")
+#
+# # Predict probabilities
+# probabilities = model.predict_proba(X_test)[:, 1]
+#
+# # Calculate accuracy
+# accuracy = get_accuracy(model, X_test, y_test)
+# print(f"Model Accuracy: {accuracy:.2f}")
+#
+# # Calculate confusion matrix
+# cm = get_confusion_matrix(model, X_test, y_test)
+# print("Confusion Matrix:")
+# print(cm)
 
-# Load or make the model
-model = get_model(data_path="bank_loan.csv")
+# Initialize Firebase Admin SDK
 
-# Predict probabilities
-probabilities = model.predict_proba(X_test)[:, 1]
+cred = credentials.Certificate("neueda-hackathon-firebase-adminsdk-fbsvc-46920acc69.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://neueda-hackathon-default-rtdb.firebaseio.com/'
+})
 
-# Calculate accuracy
-accuracy = get_accuracy(model, X_test, y_test)
-print(f"Model Accuracy: {accuracy:.2f}")
-
-# Calculate confusion matrix
-cm = get_confusion_matrix(model, X_test, y_test)
-print("Confusion Matrix:")
-print(cm)
+ref = db.reference('loans')
+data = ref.get()
+print(data)
